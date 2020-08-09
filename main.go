44a00cc3632cc09fb38d106d714fa9f4092b66f2
kooi nam ng <kooinamng@koois-MacBook-Pro.local>
2020-08-10 01:47:39 +0800

    change serve demo

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	fab "github.com/kooinam/fabio"
 	"github.com/kooinam/fabio-demo/app/controllers"
 )
@@ -12,5 +14,8 @@ func main() {
 	fab.RegisterController("player", &controllers.PlayersController{})
 	fab.RegisterController("room", &controllers.RoomsController{})
 
-	fab.Serve()
+	fab.Serve(func() {
+		fs := http.FileServer(http.Dir("./demo"))
+		http.Handle("/demo/", http.StripPrefix("/demo/", fs))
+	})
 }
